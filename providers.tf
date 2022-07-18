6c92af6fdd9c5cf77076b1f69a228bad46186ea4
nemalipurianurag <nemalipurianurag@gmail.com>
2022-07-18 12:33:22 +0530

    commiting the code without creds

--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
 provider "google" {
   project     = "modular-scout-345114"
   region      = "us-central1"
-  #credentials = file("./ca-key.json")
-  GOOGLE_CREDENTIALS = "ya29.A0AVA9y1vxDkOFCf1fImVyWQ_dO6PBZlNQW_ApzwowL8PAYSfYtjVrsjO2mBfRrgPtwsTvlza96WLfBA1i5_MwLPpw72TG57hHce1cqTso5ru-hZ5p0oxXU3ski0CtsX-xKwixOh6hECW5GAkqXlZfY5uhTmqkLvQM7TR5JLIYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4LTB5ZjZGOTFWRWNrRG1TaHUyWjg1dw0174"
+  #credentials = file("./credentials.json")
+  #GOOGLE_CREDENTIALS = "ya29.A0AVA9y1vxDkOFCf1fImVyWQ_dO6PBZlNQW_ApzwowL8PAYSfYtjVrsjO2mBfRrgPtwsTvlza96WLfBA1i5_MwLPpw72TG57hHce1cqTso5ru-hZ5p0oxXU3ski0CtsX-xKwixOh6hECW5GAkqXlZfY5uhTmqkLvQM7TR5JLIYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4LTB5ZjZGOTFWRWNrRG1TaHUyWjg1dw0174"
 }
 
 provider "google-beta" {
   project     = "modular-scout-345114"
   region      = "us-central1"
-  #credentials = file("./ca-key.json")
-  GOOGLE_CREDENTIALS = "ya29.A0AVA9y1vxDkOFCf1fImVyWQ_dO6PBZlNQW_ApzwowL8PAYSfYtjVrsjO2mBfRrgPtwsTvlza96WLfBA1i5_MwLPpw72TG57hHce1cqTso5ru-hZ5p0oxXU3ski0CtsX-xKwixOh6hECW5GAkqXlZfY5uhTmqkLvQM7TR5JLIYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4LTB5ZjZGOTFWRWNrRG1TaHUyWjg1dw0174"
+  #credentials = file("./credentials.json")
+  #GOOGLE_CREDENTIALS = "ya29.A0AVA9y1vxDkOFCf1fImVyWQ_dO6PBZlNQW_ApzwowL8PAYSfYtjVrsjO2mBfRrgPtwsTvlza96WLfBA1i5_MwLPpw72TG57hHce1cqTso5ru-hZ5p0oxXU3ski0CtsX-xKwixOh6hECW5GAkqXlZfY5uhTmqkLvQM7TR5JLIYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4LTB5ZjZGOTFWRWNrRG1TaHUyWjg1dw0174"
 }
